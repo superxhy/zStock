b7e744c1e2b75b5cb82d15655a68a50d92735046
superxhy <yql_19840@163.com>
2017-07-21 11:18:47 +0800

    add profile and prettytable for md

--- until/tools.py
+++ until/tools.py
@@ -7,8 +7,8 @@ Created on 2017-5-5
 '''
 
 from prettytable import PrettyTable
-from .html5.pyhwraper import HtmlPageMaker
-#from pyhwraper import HtmlPageMaker
+from .html5.pyhwraper import HtmlPageMaker,quote
+#from pyhwraper import HtmlPageMaker,quote
 from .sender import MailSender
 #from sender import MailSender
 '''
@@ -76,6 +76,66 @@ def getMarkDownTable(dictList, schema=[]):
         i += 1
     return "".join(result)
 
+def getMarkDownTitle(title,sub=1):
+    if type(title) != str or len(title) == 0:
+        return ''
+    result = '#'
+    for i in range(1,sub):
+        result += '#'
+    result += ' '
+    result += title
+    result += '\n'
+    return result
+    
+def getMarkDownTableLine(dicttab, schema=[]):
+    if type(dicttab) != dict:
+        return ''
+    result = ''
+    hrule = '---'
+    result += hrule
+    result += '\n'
+    row = []
+    for colum in schema:
+        row.append(colum)
+        row.append(': ')
+        row.append(str(dicttab.get(colum,'-')))
+        row.append('\n')
+    result += ("".join(row))
+    result += hrule
+    result += '\n'
+    return result
+
+def getMarkDownTableFromPretty(tab):
+    if tab == None:
+        return ''
+    datastr = tab.get_string()
+    junction_char = '+'
+    headerjunction = junction_char + '\n'
+    headerindex = datastr.find(headerjunction)
+    if headerindex < 0 :
+        return ''
+    hrule = datastr[:headerindex+1]
+    substr = datastr[headerindex+2:]
+    sp = substr.split(hrule)
+    if len(sp) == 0 :
+        return ''
+    header = sp[0]
+    content =sp[1].strip(hrule)
+    border = hrule.replace(junction_char, '|')
+    return header + border + content
+
+def getBlogMd(title,category,comments,tab,tabname=''):
+    mdtable  = getMarkDownTableFromPretty(tab)
+    mdtitle = getMarkDownTitle(tabname)
+    comments = 'false'
+    mdprof = getMarkDownTableLine({'layout':'post','title':title,'category':category,'comments':comments},
+        ['layout','title','category','comments'])
+    mdblog = mdprof + mdtitle + mdtable
+    return mdblog
+    
+def quoteHtml(s):
+    return quote(s)
+
 def getHtmlTable(dictList, schema):
     maker = HtmlPageMaker()
     maker.addTable(dictList,schema)
@@ -90,9 +150,10 @@ def sendTable(subject, dictList, schema, config=None):
     maker = getHtmlTable(dictList, schema)
     #fname = maker.getTitle() + '.html'
     fname = 'curattach.html'
-    tabcontent = str(getPrettyTable(dictList, schema))
-    tabcontent += '\n'
-    tabcontent += getMarkDownTable(dictList, schema)
+    tab = getPrettyTable(dictList, schema)
+    title = maker.getTitle()
+    comments = 'false'
+    mdblog = getBlogMd(title, 'stock', comments, tab)
     '''maker.getHtml()'''
-    sendHtmlMail(subject, tabcontent , [maker.saveFile(fname)], config)
-    return tabcontent
+    sendHtmlMail(subject, quoteHtml(mdblog) , [maker.saveFile(fname)], config)
+    return mdblog
